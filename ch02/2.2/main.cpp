59e872c1ee23ffee8ae6e7c8c14771f5f932f3bd
Sal Gomez <svtgomez@gmail.com>
2014-12-28 16:48:30 -0800

    added much better (and simpler) solution for 2.2

--- ch02/2.2/main.cpp
+++ ch02/2.2/main.cpp
@@ -5,6 +5,9 @@
 
 using namespace std;
 
+/*
+    this uses O(n) space and O(n+k) time, we can do better
+*/
 int findKthToLast(Node* n, int k)
 {
     if (k<1) {
@@ -46,6 +49,37 @@ int findKthToLast(Node* n, int k)
     return retNode->data;
 }
 
+/*
+    sneaky but easy technique to find in O(n) and constant mem, just use two counters
+*/
+int findKthToLast2(Node* n, int k)
+{
+    if ( (!n) || (k<1) ) {
+        printf("args error in findKthToLast2()\n");
+        return -1;;
+    }
+    Node* nl = n; //leader node
+    Node* nf = n; //follower node, will end up as target node k
+
+    while (k>0) {
+        if (!nl) {
+            printf("not enough nodes\n");
+            return -1;
+        }
+        nl = nl->next;
+        k--;
+    }
+
+    while (nl) {
+        nl = nl->next;
+        nf = nf->next;
+    }
+
+    printf("retNode: %p val: %d\n", nf, nf->data);
+    return nf->data;
+}
+
+
 int main(void)
 {
     printf("in main\n");
@@ -57,11 +91,18 @@ int main(void)
     Node* n2 = new Node(2, n3);
     Node* n1 = new Node(1, n2);
     printSLL(n1);
+    /*
     findKthToLast(n1, 1);
     findKthToLast(n1, 5);
     findKthToLast(n1, 2);
     findKthToLast(n1, 0);
     findKthToLast(n1, 6);
+    */
+    findKthToLast2(n1, 1);
+    findKthToLast2(n1, 5);
+    findKthToLast2(n1, 2);
+    findKthToLast2(n1, 0);
+    findKthToLast2(n1, 6);
     delete n5;
     delete n4;
     delete n3;
