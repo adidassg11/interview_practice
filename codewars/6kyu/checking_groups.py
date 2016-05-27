0c4fb9d409386a08019600c61eb44a6b572f1cde
Joe Doe <svtgomez@gmail.com>
2016-05-26 18:44:53 -0700

    adding codewars

---
+++ codewars/6kyu/checking_groups.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+"""
+http://www.codewars.com/kata/54b80308488cb6cd31000161/train/python
+
+In English and programming, groups can be made using symbols such as "()" and "{}" that change meaning. However, these groups must be closed in the correct order to maintain correct syntax.
+
+Your job in this kata will be to make a program that checks a string for correct grouping. For instance, the following groups are done correctly:
+
+({})
+[[]()]
+[{()}]
+
+The next are done incorrectly
+
+{(})
+([]
+[])
+
+"""
+
+def group_check(s):
+    pass
