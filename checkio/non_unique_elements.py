810865b7cb1af7061ff020a728791e06524c80f1
Joe Doe <svtgomez@gmail.com>
2017-02-23 20:11:08 -0800

    cool website checkio

---
+++ checkio/non_unique_elements.py
@@ -0,0 +1,17 @@
+
+
+# Data set < 10000 entries so O(n^2) should be fine
+def checkio(data):
+    non_uniques_list = [x for x in data if data.count(x) > 1]
+    print('non uniques: %s' % non_uniques_list)
+
+    return non_uniques_list
+
+
+if __name__ == "__main__":
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert isinstance(checkio([1]), list), "The result must be a list"
+    assert checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3], "1st example"
+    assert checkio([1, 2, 3, 4, 5]) == [], "2nd example"
+    assert checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5], "3rd example"
+    assert checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9], "4th example"
