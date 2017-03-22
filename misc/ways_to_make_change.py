5d8867c1846322b572028531f60ac81ef4412744
Joe Doe <svtgomez@gmail.com>
2017-03-22 14:30:36 +0100

    some cleanup

--- misc/ways_to_make_change.py
+++ misc/ways_to_make_change.py
@@ -18,11 +18,10 @@ def clean_print(coins):
     print('%s Quarters %s Dimes %s Nickels %s Pennies' % (cc['Q'], cc['D'], cc['N'], cc['P']))
 
 
-#slow AF around 49 (over 1M combs), maybe there are repeats, try with set...
+# Slow AF around 49 (over 1M combs), maybe there are repeats, try with set...
 def ways_to_make_change1(max_price):
     change_combs = {0: ['']}  # combinations to make change for a given price e.g. 5: ['N', 'PPPPP']
 
-    #import pdb; pdb.set_trace()  # DEBUG
     for price in range(1, max_price+1):
         print('Price: %s' % price)
         new_price_combs = []
@@ -42,24 +41,20 @@ def ways_to_make_change1(max_price):
             print('combs for %s cents: %s' % (price, new_price_combs))
 
 
-# Same speed and number of combinations
+# Huge speedup, num combinations is in the 3 digits range, not millions. Set avoids repeats
 def ways_to_make_change2(max_price):
     change_combs = {0: set(' ')}  # combinations to make change for a given price e.g. 5: ['N', 'PPPPP']
 
-    #import pdb; pdb.set_trace()  # DEBUG
     for price in range(1, max_price+1):
-        print('Price: %s' % price)
         new_price_combs = set()
 
         for coin in ordered_coin_vals:
             if price-coin['value'] in change_combs:
                 for comb in change_combs[price-coin['value']]:
-                    new_price_comb = ''.join(sorted(comb + coin['name']))  # TODO: give a key function to sort by QNDP order
+                    new_price_comb = ''.join(sorted(comb + coin['name']))  # TODO: give a key function to sort by QNDP order... can just use bash sort on output tho...
                     new_price_combs.add(new_price_comb)
 
         change_combs[price] = new_price_combs
-
-        print('combinations: %s' % len(change_combs[price]))
 
         if price == 100:
             # sort my set
@@ -67,8 +62,6 @@ def ways_to_make_change2(max_price):
             max_len = 0
             for x in sorted_list:
                 max_len = max(max_len, len(x))
-            print('max len: %s' % max_len)
-            #print('combs for %s cents: %s' % (price, new_price_combs))
             for comb in new_price_combs:
                 clean_print(comb)
 
